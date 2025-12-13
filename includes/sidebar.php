0649a7eab9e015bd00bb4df33544cd8518f9526f
cheeyinnn <cheeyin@1utar.my>
2025-12-13 22:31:42 +0800

    test

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -153,17 +153,12 @@ $teamPendingCountSidebar = $teamPendingCountSidebar ?? 0;
                 <iconify-icon icon="solar:cup-star-line-duotone"></iconify-icon>
                 Leaderboard
             </a>
-
-            <a href="LbDetail.php" class="sidebar-link <?= isActive('LbDetail.php') ?>">
-                <iconify-icon icon="solar:users-group-two-rounded-line-duotone"></iconify-icon>
-                lbdetail
-            </a>
         
             <div class="sidebar-nav-title">Redemption</div>
 
             <a href="rewards.php" class="sidebar-link <?= isActive('rewards.php') ?>">
                 <iconify-icon icon="solar:gift-line-duotone"></iconify-icon>
-                rewards
+                Rewards
             </a>
 
             <div class="sidebar-nav-title">EcoTrip</div>
